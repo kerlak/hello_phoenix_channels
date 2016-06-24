9794a2b673879e03c91e96418537c8a190a5938e
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2016-06-24 22:00:15 +0200

    handle messages

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -7,4 +7,16 @@ defmodule HelloPhoenixChannels.RoomChannel do
   def join("rooms:"<> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body}
+    {:noreply, socket}
+  end
+
+  def handle_out("new_msg", payload, socket) do
+    push socket, "new_msg", payload
+    {:noreply, socket}
+  end
+
+
 end
